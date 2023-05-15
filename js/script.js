6e1943225c40d644f4359c760a1344d03ea3f491
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-15 17:04:47 +0200

    trying to resolve discount

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 const userJourney = document.getElementById("userInputJourney");
 
-const userAge = parseInt(document.getElementById("userInputAge"));
+const userAge = document.getElementById("userInputAge");
 
 const formButton = document.getElementById("formInputButton");
-console.log(userJourney.value)
+
 let ticketPrice = userJourney.value * 0.233;
 
+parseInt(userJourney)
+
+parseInt(userAge)
+
+const underAgeDiscount = ticketPrice * (19.4 / 100);
+
+const overSixtyfiveDiscount = ticketPrice * (37.7 / 100)
+
 formButton.addEventListener("click", function(){
+        if (userAge < 18){
+            ticketPrice = ticketPrice - underAgeDiscount;
+        };
+
         console.log(ticketPrice);
+        
+
+        
     }
 );
 
