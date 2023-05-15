0b3fb3f2b3f3825dd3931171cf726d754b96dc04
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-15 17:14:43 +0200

    first milestone reached

--- js/script.js
+++ js/script.js
@@ -10,19 +10,22 @@ parseInt(userJourney)
 
 parseInt(userAge)
 
+console.log(ticketPrice)
+
 const underAgeDiscount = ticketPrice * (19.4 / 100);
 
 const overSixtyfiveDiscount = ticketPrice * (37.7 / 100)
 
 formButton.addEventListener("click", function(){
-        if (userAge < 18){
+        if (userAge.value < 18){
             ticketPrice = ticketPrice - underAgeDiscount;
-        };
+        }
+
+        else if (userAge.value >= 65){
+            ticketPrice = ticketPrice - overSixtyfiveDiscount;
+        }
 
         console.log(ticketPrice);
-        
-
-        
     }
 );
 
