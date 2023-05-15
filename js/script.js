ed0ad5c6b3bf03965d206eba5a99fb9ac3346a7f
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-15 16:32:50 +0200

    operation now working

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const userJourney = parseInt(document.getElementById("userInputJourney"));
+const userJourney = document.getElementById("userInputJourney");
 
 const userAge = parseInt(document.getElementById("userInputAge"));
 
 const formButton = document.getElementById("formInputButton");
-
+console.log(userJourney.value)
 let ticketPrice = userJourney.value * 0.233;
 
 formButton.addEventListener("click", function(){
