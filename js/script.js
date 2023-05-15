cb77cdfacc017bcf8fea80d5d60546328d8c9929
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-15 15:55:57 +0200

    changed first input text to input number

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 const userJourney = parseInt(document.getElementById("userInputJourney"));
+
 const userAge = parseInt(document.getElementById("userInputAge"));
+
+const formButton = document.getElementById("formInputButton");
+
+let ticketPrice = userJourney * 0.233;
+
+formButton.addEventListener("click", function(){
+        console.log(ticketPrice);
+    }
+);
+
